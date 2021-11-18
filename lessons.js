b08b98715c2127fba76f270a75ad8515fdb08a67
Muhfszxx <93323446+Muhfszxx@users.noreply.github.com>
2021-11-18 21:28:48 +0100

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,110 @@
+let lessons = [
+    {
+        "id": 01,
+        "subject": 'Accounting',
+        "location": "Mauritius",
+        "price": 300,
+        "image": "img/accounting.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 02,
+        "subject": 'Chemistry',
+        "location": "Dubai",
+        "price": 250,
+        "image": "img/chemistry.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 03,
+        "subject": 'Biology',
+        "location": "Hendon",
+        "price": 200,
+        "image": "img/biology.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 04,
+        "subject": 'Government',
+        "location": "Mauritius",
+        "price": 150,
+        "image": "img/government.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 05,
+        "subject": 'English',
+        "location": "Dubai",
+        "price": 200,
+        "image": "img/english.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 06,
+        "subject": 'Law',
+        "location": "Hendon",
+        "price": 350,
+        "image": "img/law.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 07,
+        "subject": 'History',
+        "location": "Mauritius",
+        "price": 170,
+        "image": "img/history.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 08,
+        "subject": 'Science',
+        "location": "Dubai",
+        "price": 240,
+        "image": "img/science.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 09,
+        "subject": 'Math',
+        "location": "Hendon",
+        "price": 190,
+        "image": "img/math.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 010,
+        "subject": 'Mechanics',
+        "location": "Mauritius",
+        "price": 200,
+        "image": "img/demo-img.svg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 011,
+        "subject": 'Auto repair',
+        "location": "Dubai",
+        "price": 300,
+        "image": "img/demo-img.svg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 012,
+        "subject": 'Civil Engineering',
+        "location": "Hendon",
+        "price": 260,
+        "image": "img/demo-img.svg",
+        "availableInventory":5,
+        "rating": 5
+    }
+]
